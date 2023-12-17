c231245820d0981ce327f21d3bf62fa092bb5219
nimitdesai2000 <nimit.desai@agami-tech.com>
2023-12-17 14:47:09 +0530

    Article Attachement Added

--- routes/articleRoutes.js
+++ routes/articleRoutes.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const { getArticle,CreateArticle,deleteArticle,uploadAttachements } = require('../controllers/articleController')
 require('dotenv').config()
+const fs = require('fs');
 
 const router = express.Router()
 const multer = require("multer");
 
 const storage = multer.diskStorage({
     destination:function(req,file,cb) {
-        return cb(null,process.env.FILE_UPLOAD_PATH);
+        const path = process.env.FILE_UPLOAD_PATH+"/"+req.params.uuid;
+        fs.mkdirSync(path,{recursive:true})
+        return cb(null,process.env.FILE_UPLOAD_PATH+"/"+req.params.uuid);
     },
     filename:function(req,file,cb) {
         return cb(null,`${file.originalname}`)
@@ -20,6 +23,6 @@ router.get('/getallarticle/:id', getArticle);
 router.post('/create-new-article', CreateArticle);
 router.get('/getallarticle', getArticle);
 router.delete('/deletearticle/:id',deleteArticle);
-router.post('/upload-attachements',upload.single('file'), uploadAttachements);
+router.post('/upload-attachements/:uuid',upload.single('file'), uploadAttachements);
 
 module.exports = router
